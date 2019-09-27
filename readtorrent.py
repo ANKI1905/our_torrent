b5218ad3e2b79dbf7420191b7175cd27ac9fe2fc
ANKI1905 <ankitamvaid12@gmail.com>
2019-09-27 12:50:07 +0530

    get the chunks from peers

--- readtorrent.py
+++ readtorrent.py
@@ -22,26 +22,39 @@ class readtorrent:
 		self.source = ""
 		self.hash_info = ""
 		self.getinfo(sys.argv[1])
-		self.peer_id = "torentorentorentoren"
+		self.peer_id = "QWERTYUIOPASDFGHJKLZ"
+		self.directory = " "
+		self.files = []
+		self. no_of_piece = 0 
 
 	def getinfo(self, filename):
 		fp = open(filename, "rb")
 		data = fp.read()	#it is string
 		decode_data = bencoder.decode(data)    #get a dictionary 
 		#print(decode_data)
-		if 'announce-list' in decode_data and 'info' in decode_data:
+		if 'announce' in decode_data and 'info' in decode_data:
 			self.tracker = decode_data.get('announce')
 			self.tracker_list = decode_data.get('announce-list')
 			self.create = decode_data.get('created by')
 			self.date = decode_data.get('creation date')
 			self.source = decode_data.get('comment')
 			info = decode_data.get('info')
-			self.size = info.get('length')
 			self.piece_size = info.get('piece length')
-			self.name = info.get('name')
 			self.pieces = info.get('pieces')
 		else:
 			print("Invalid torrent file")
+		if 'length' in info:
+			self.directory = ''
+			self.files = [(info.get('name'), info.get('length'))]
+			self.size = info.get('length')
+			self.name = info.get('name')
+		else:
+			files = []			
+			self.directory = info.get('name')
+			for d in decode_data['info']['files']:
+				files.append((d['path'], d['length']))
+				self.size += d['length']
+		self.no_of_pieces = self.size / self.piece_size
 		sha1 = hashlib.sha1()
 		sha1.update(bencoder.encode(info))
 		self.hash_info = sha1.digest()
@@ -92,7 +105,7 @@ def announce_udptracker(resp_buf, mytorrent):
 	key = 0
 	num_want = -1
 	port = 6881
-	final_buf = struct.pack('qqqiIIih', downloaded, left, uploaded, event, ip, key, num_want, port)
+	final_buf = struct.pack('!qqqiIIih', downloaded, left, uploaded, event, ip, key, num_want, port)
 	buffer_data = conn_id + initial_buf + mytorrent.hash_info + mytorrent.peer_id + final_buf
 	
 	return buffer_data
@@ -121,14 +134,19 @@ def connect_udptracker(mytorrent, tracker):
 		mysocket.settimeout(5)
 		urlsep = urlparse.urlparse(tracker)
 		port = urlsep.port
-		ip = socket.gethostbyname(urlsep.hostname)
+		try:
+			ip = socket.gethostbyname(urlsep.hostname)
+		except socket.gaierror as e:
+			print(e)
+			return
 		conn_id = 0x41727101980
 		action = 0x0
 		transaction_id = int(random.randrange(0, 255))
-		conn_buf = struct.pack('!Qii', conn_id, action, transaction_id) #conn_id is 64bit, action and transaction_id 32 bit standard
+		conn_buf = struct.pack('!Qii', conn_id, action, transaction_id) 
 		try:	
-				
+			print('in try')	
 			mysocket.sendto(conn_buf, (ip, port))
+			print('conn')
 			response = mysocket.recvfrom(2048)
 			print("udp")
 			resp_buf = response[0]
@@ -144,55 +162,189 @@ def connect_udptracker(mytorrent, tracker):
 			mysocket.close()
 			return peers
 		
+		except socket.gaierror as e:
+			print(e)
+			sys.exit()
+
 		except Exception as e:
             		print(e)
             		return
-		except socket.gaierror as e:
-			print(e)
-			return
-
 
 def getpeers(mytorrent):
 	peers_list = []
-	for i in mytorrent.tracker_list:
-		print(i)
-		tracker = i[0][0:]
+	if mytorrent.tracker_list != None:
+		for i in mytorrent.tracker_list:
+			print(i)
+			tracker = i[0][0:]
+			if i[0][:4] == 'http':
+				peers = connect_httptracker(mytorrent, tracker)
+			elif i[0][:3] == 'udp':
+				peers = connect_udptracker(mytorrent, tracker)
+			if peers_list != None and peers != None:
+				for j in peers:
+					if j not in peers_list:
+						peers_list.append(j)
+			elif peers_list == None: 
+				peers_list = peers
+	
+	else:
+		i = mytorrent.tracker
+		print i
 		if i[0][:4] == 'http':
 			peers = connect_httptracker(mytorrent, tracker)
 		elif i[0][:3] == 'udp':
 			peers = connect_udptracker(mytorrent, tracker)
-		if peers not in peers_list:
-			peers_list.append(peers)
+		peers_list = peers
 	
+	print peers_list
 	if len(peers_list) != 0:
-		return peers_list
+		return peers_list		
+
+def keep_alive():
+	length = 0
+	message = struct.pack("!i", length)
+	return message
+def choke():
+	message_id = 0
+	length = 1
+	message = struct.pack('!ib', length, message_id)
+	return message
+def unchoke():
+	message_id = 1
+	length = 1
+	message = struct.pack('!ib', length, message_id)
+	return message
+def pack_interested():
+	message_id = 2
+	length = 1
+	message = struct.pack('!ib', length, message_id)
+	print (binascii.hexlify(message))
+	return message
+
+
+def not_interested():
+	message_id = 3
+	length = 1
+	message = struct.pack('!ib', length, message_id)
+	return message
+
+
+def unpack_unchoke(response):
+	print response[0:4]
+	length = response[0:4]
+	message_id = response[4:5]
+	print('ANKITA')
+	print(length, message_id)
+	return True
+
+
+def pack_request(mytorrent):
+	length = 13
+	message_id = 6
+	index = 0
+	begin = 0
+	if mytorrent.piece_size > 16384:
+		blength = 16384
+	else:
+		blength = mytorrent.piece_size
+	message = struct.pack('!ibiii', length, message_id, index, begin, blength)
+	return message
+
+
+
+
+
+
 
 def connect_handshake(peers_list, mytorrent):
-	print peers_list
 	for i in peers_list:
-		print i
 		ip = i[0]
-		port = i[1]
-		print ip
-		print port
+		port = i[1][-1]
 		peerSocket = socket.socket()
-		peerSocket.connect(i[0])
+		peerSocket.settimeout(5)
+		try:			
+			peerSocket.connect((ip, port))
+			print(ip, port)
+			print('CONNECTION')
+		except Exception as e:
+			print (e) 
+			pass
 		
-		pstr = "BitTorrent protocol"
-		pstrlen = len(pstr)
-		reserved = struct.pack('!q', 0)
-		message = pstrlen + pstr + reserved + mytorrent.info_hash + mytorrent.peer_id
-		print(message)
+		pstr = b"BitTorrent protocol"
+		pstrlen = chr(len(pstr))
+		reserved = "\x00\x00\x00\x00\x00\x00\x00\x00"
+		message = pstrlen + pstr + reserved + mytorrent.hash_info + mytorrent.peer_id
+		#print(message)
+		try:			
+			peerSocket.send(message)
+			print('SENd')
+		except Exception as e:
+			print (e) 
+			pass
+		response_ =[]
+		try:			
+			response_ = peerSocket.recv(2048)
+			'''a = len(message)
+			print a
+			print(len(response_))
+			response = response_[len(message) :]
+			print(len(response))
+			print response
+			if len(response) > 0:
+				length_prefix = struct.unpack('!i',response[0 : 4])
+				bitlength = length_prefix - 1
+				print length_prefix
+				id_ = struct.unpack('!B', response[4:5])
+				print id_
+				b = 5
+				bitfield = response[5 : ]
+				print(bitfield)
+				print(bitfield.decode())'''
+		except Exception as e:
+			print (e) 
+			pass
+		response = response_[len(message) :]
+		if len(response) > 0:
+			length_prefix = struct.unpack('!i',response[0 : 4])
+			id_ = struct.unpack('!B', response[4:5])
+			blength = length_prefix[0] - 1
+			b = 5
+			bitfield = response[5 : blength + 5 ]
+			bytestring = binascii.hexlify(bitfield)
+			print(" ".join([bytestring[i:i+2] for i in range(0, len(bytestring), 2)]))
+			if len(bitfield) == blength:
+				intrst = pack_interested()
+				try:
+					peerSocket.send(intrst)
+					rsponse = peerSocket.recv(2048)
+					print(len(rsponse))
+					b = unpack_unchoke(rsponse)
+					if b:
+						try:
+							print 'SEND REQ'
+							peerSocket.send(pack_request(mytorrent))
+							rp = peerSocket.recv(2048)
+							print rp
+							print('MESSAGE')
+							print (struct.unpack('!i',rp [0:4]))
+							print (struct.unpack('!b', rp[4:5]))
+							print rp[5:]
+						except:
+							pass
+						
+				except:
+					pass
+				
+
+
+		
+		peerSocket.close()
 		
 
 
 mytorrent = readtorrent()
 #print(mytorrent.tracker_list)
 peers_list = getpeers(mytorrent)
-print 'loop printing'
-print peers_list[0][:1]
-for i in peers_list:
-	print i
-	break
+
 #print(peers_list)
-#connect_handshake(peers_list, mytorrent)
+connect_handshake(peers_list, mytorrent)
